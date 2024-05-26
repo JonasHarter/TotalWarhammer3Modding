870524ea2c7a129a6bd4d84d66b4581c67903d81
Jonas Harter <github@hepatizon.de>
2024-05-26 14:39:48 +0200

    Added unit cost breakdown

--- AnotherUnitCapsMod/pack/script/campaign/mod/kafka_aucm_tooltip_army.lua
+++ AnotherUnitCapsMod/pack/script/campaign/mod/kafka_aucm_tooltip_army.lua
@@ -1,33 +1,34 @@
-local aucm = core:get_static_object("aucm");
+local aucm = core:get_static_object("aucm")
 
 -- Set army cost tooltip
 core:add_listener(
 	"kafka_aucm_setArmyCostTooltip",
 	"CharacterSelected", 
     function(context)
-      return context:character():has_military_force();
+      return context:character():has_military_force()
     end,
 	function(context)
-		local character = context:character();
-		cm:set_saved_value("aucm_last_selected_char_cqi", character:command_queue_index());
+		local character = context:character()
+		cm:set_saved_value("aucm_last_selected_char_cqi", character:command_queue_index())
 		cm:callback( function()
-			aucm:setArmyCostToolTip(character);
+			aucm:setArmyCostToolTip(character)
+			aucm:setUnitCostBreakdownTooltip(character)
 		end, 0.1)
 	end,
 	true
-);
+)
 
 core:add_listener(
 	"kafka_aucm_clearLastSelectedCharacter",
 	"CharacterSelected",
 	function(context)
-		return not context:character():has_military_force();
+		return not context:character():has_military_force()
 	end,
 	function()
-		cm:set_saved_value("aucm_last_selected_char_cqi", "");
+		cm:set_saved_value("aucm_last_selected_char_cqi", "")
 	end,
 	true
-);
+)
 
 -- Catch all clicks to refresh the army cost tt if the units_panel is open
 -- Fires also when player cancels recruitment of a unit, adds a unit to the queue etc
@@ -35,7 +36,7 @@ core:add_listener(
 	"kafka_aucm_setArmyCostTooltip_clicked",
 	"ComponentLClickUp",
 	function(context)
-		return cm.campaign_ui_manager:is_panel_open("units_panel");
+		return cm.campaign_ui_manager:is_panel_open("units_panel")
 	end,
 	function(context)
 		cm:callback(function()
@@ -43,7 +44,7 @@ core:add_listener(
 			if not savedCqi then
 				return
 			end
-			local lastSelectedCharacter = cm:get_character_by_cqi(cm:get_saved_value("aucm_last_selected_char_cqi"));
+			local lastSelectedCharacter = cm:get_character_by_cqi(cm:get_saved_value("aucm_last_selected_char_cqi"))
 			if not(lastSelectedCharacter and lastSelectedCharacter ~= "") then
 				return
 			end
@@ -53,33 +54,67 @@ core:add_listener(
 			if not cm:char_is_mobile_general_with_army(lastSelectedCharacter) then
 				return
 			end
-			aucm:setArmyCostToolTip(lastSelectedCharacter);
+			aucm:setArmyCostToolTip(lastSelectedCharacter)
+			aucm:setUnitCostBreakdownTooltip(lastSelectedCharacter)
 		end, 0.3)
 	end,
 	true
-);
+)
 
 -- Shows the army cost in the army name tooltip
 function aucm:setArmyCostToolTip(character)
-  local zoom_component = find_uicomponent(core:get_ui_root(), "main_units_panel", "button_focus");
+  local zoom_component = find_uicomponent(core:get_ui_root(), "main_units_panel", "button_focus")
 	if not zoom_component then
-		return;
+		return
 	end
 
 	local tt_text = ""
 	if character:has_military_force() then
-		local armyCost = aucm:getArmyCost(character);
-		local armyLimit = aucm:getArmyLimit(character);
-		local armyQueueCost = aucm:getArmyQueuedUnitsCost();
+		local armyCost = aucm:getArmyCost(character)
+		local armyLimit = aucm:getArmyLimit(character)
+		local armyQueueCost = aucm:getArmyQueuedUnitsCost()
 		tt_text = "Current Cost: " .. armyCost .. "/" .. armyLimit
 		tt_text = tt_text .. "\n"
 		if armyQueueCost > 0 then
 			tt_text = tt_text .. "Expected Cost: " .. (armyCost + armyQueueCost) .. "/" .. armyLimit
+			tt_text = tt_text .. "\n"
 		end
 		local armyHeroCount = aucm:getArmyHeroCount(character)
 		local armyHeroLimit = aucm:getConfigArmyLimitHeroCap()
-		tt_text = tt_text .. "\n"
 		tt_text = tt_text .. "Heroes: " .. armyHeroCount .. "/" .. armyHeroLimit
 	end
-	zoom_component:SetTooltipText(tt_text, true);
+	zoom_component:SetTooltipText(tt_text, true)
+end
+
+function aucm:setUnitCostBreakdownTooltip(character)
+	local infoButton = find_uicomponent(core:get_ui_root(), "units_panel", "main_units_panel", "tr_element_list", "button_info_holder", "button_info")
+	if not infoButton then
+		return
+	end
+
+	local unitCosts = {}
+	local unitList = character:military_force():unit_list()
+	for i = 0, unitList:num_items() - 1 do
+		local unit = unitList:item_at(i)
+		unitCosts[unit:unit_key()] = aucm:getUnitCost(unit)
+	end
+
+	infoButton:SetTooltipText(aucm:getUnitListCostTooltip(unitCosts), true)
+end
+
+function aucm:getUnitListCostTooltip(unitCosts)
+	local tt_text = "Unit costs breakdown:\n"
+	local unitCostsKeys = aucm:getTableKeys(unitCosts)
+	table.sort(unitCostsKeys, function(keyLhs, keyRhs)
+		return unitCosts[keyLhs] < unitCosts[keyRhs]
+	end)
+	for _, unitKey in pairs(unitCostsKeys) do
+		local unitCost = unitCosts[unitKey]
+		local unitName = common.get_localised_string("land_units_onscreen_name_" .. unitKey)
+		if not aucm:isHero(unitKey) then
+			tt_text = tt_text .. unitName .. ": " .. unitCost .. "\n"
+		end
+	end
+
+	return tt_text
 end
--- AnotherUnitCapsMod/pack/script/mct/settings/kafka_aucm_mct_settings.lua
+++ AnotherUnitCapsMod/pack/script/mct/settings/kafka_aucm_mct_settings.lua
@@ -1,7 +1,11 @@
-if not get_mct then return end
+if not get_mct then
+	return
+end
 local mct = get_mct();
 
-if not mct then return end
+if not mct then
+	return
+end
 local mct_mod = mct:register_mod("kafka_another_unit_caps_mod");
 
 mct_mod:set_title("Cost-based Army Caps", false);
@@ -12,13 +16,9 @@ mct_mod:add_new_section("aucm_base", "Base Options", false);
 
 local option_cbac_army_limit_player = mct_mod:add_new_option("army_limit_point_divider", "slider");
 option_cbac_army_limit_player:set_text("Army limit");
-option_cbac_army_limit_player:set_tooltip_text(
-    "The amount the mp-cost of the units get divided by to reach the actual point value (rounded down).\n" ..
-    "[[col:yellow]]" .. 
-    "All other options have to be adjusted manually with this value in mind.\n" .. 
-    "An army limit of 50 with a divider of 200 equals a limit of 10000 with a divider of 1." ..
-    "[[/col]]"
-);
+option_cbac_army_limit_player:set_tooltip_text("The amount the mp-cost of the units get divided by to reach the actual point value (rounded down).\n" .. "[[col:yellow]]" ..
+				                                               "All other options have to be adjusted manually with this value in mind.\n" ..
+				                                               "An army limit of 50 with a divider of 200 equals a limit of 10000 with a divider of 1." .. "[[/col]]");
 option_cbac_army_limit_player:slider_set_min_max(1, 1000);
 option_cbac_army_limit_player:slider_set_step_size(50);
 option_cbac_army_limit_player:set_default_value(200);
@@ -32,20 +32,14 @@ option_cbac_army_limit_player:set_default_value(50);
 
 local option_cbac_army_limit_ai = mct_mod:add_new_option("army_limit_ai_adjust", "slider");
 option_cbac_army_limit_ai:set_text("AI army limit bonus");
-option_cbac_army_limit_ai:set_tooltip_text(
-    "The amount of points, that gets applied to the army limit for ai armies.\n" .. 
-    "[[col:yellow]]A smaller value will cause AI turns to take longer.[[/col]]"
-);
+option_cbac_army_limit_ai:set_tooltip_text("The amount of points, that gets applied to the army limit for ai armies.\n" .. "[[col:yellow]]A smaller value will cause AI turns to take longer.[[/col]]");
 option_cbac_army_limit_ai:slider_set_min_max(-40000, 40000);
 option_cbac_army_limit_ai:slider_set_step_size(1);
 option_cbac_army_limit_ai:set_default_value(10);
 
 local option_cbac_hero_cap = mct_mod:add_new_option("army_limit_hero_cap", "slider");
 option_cbac_hero_cap:set_text("Army hero count");
-option_cbac_hero_cap:set_tooltip_text(
-    "The amount heroes allowed per army." ..
-    "[[col:yellow]]Applies to the player only.[[/col]]"
-);
+option_cbac_hero_cap:set_tooltip_text("The amount heroes allowed per army." .. "[[col:yellow]]Applies to the player only.[[/col]]");
 option_cbac_hero_cap:slider_set_min_max(0, 19);
 option_cbac_hero_cap:slider_set_step_size(1);
 option_cbac_hero_cap:set_default_value(2);
